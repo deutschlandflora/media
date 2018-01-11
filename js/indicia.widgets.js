9907eaa0c9ebca2b6c054915f228076b75fb0776
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-01-11 11:32:49 +0000

    Work on autocomplete select event and storing of value

--- js/indicia.widgets.js
+++ js/indicia.widgets.js
@@ -6,18 +6,33 @@ $.widget("indicia.indiciaAutocomplete", $.ui.autocomplete, {
   options: {
     source: function(term, callback) {
       var params = {};
-      $.extend(params, this.options.extraParams, {q: term.term});
+      $.extend(params, this.options.extraParams, {q: term.term, limit: 100});
       $.getJSON(this.options.baseUrl, params, callback);
-    },
+    }/*,
     response: function(event, ui) {
+      // Clear the current selected key as the user has changed the search text.
+      $('#' + $(this).attr('data-id-input').replace(/:/g, '\\:')).val('');
       console.log('response is here');
-    }
+    }*/
   },
 
-  _init: function() {
+  _create: function() {
+    this._super();
+    this._on(this.element, {
+      indiciaautocompleteselect: this._onSelect
+    });
+  },
+
+  _onSelect: function(event, ui) {
+    $('#' + this.options.id.replace(/:/g, '\\:')).val(ui.item[this.options.valueField]);
+    $(this.element).val(ui.item[this.options.captionField]);
+    console.log('Item selected');
+  },
+
+  /*_init: function() {
     console.log('In init of our widget');
     this._super();
-  },
+  },*/
 
   _renderItem: function(ul, item) {
     display = typeof item.highlighted === "undefined" ? item[this.options.captionField] : item.highlighted;
@@ -27,8 +42,8 @@ $.widget("indicia.indiciaAutocomplete", $.ui.autocomplete, {
     // @todo Species formattings, standard formatting, documentation for adding new formatter
 
     return $( "<li>" )
-      .attr( "data-value", item.id )
-      .append(display)
+      .attr( "data-value", item[this.options.valueField] )
+      .append(item[this.options.captionField])
       .appendTo( ul );
   }
 
