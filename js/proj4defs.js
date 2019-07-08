de9d693effa8f4a2df1424b7f22872cdc502a05f
Andrew van Breda <support@avb-it.co.uk>
2019-07-08 09:05:05 +0100

    Remove unusued 32633 definition

--- js/proj4defs.js
+++ js/proj4defs.js
@@ -10,4 +10,3 @@ Proj4js.defs['EPSG:23030'] = "+proj=utm +zone=30 +ellps=intl +units=m +no_defs +
 Proj4js.defs["EPSG:31466"] = "+proj=tmerc +lat_0=0 +lon_0=6 +k=1 +x_0=2500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs";
 Proj4js.defs["EPSG:31467"] = "+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs";
 Proj4js.defs["EPSG:31468"] = "+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs";
-Proj4js.defs["EPSG:32633"] = "+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
