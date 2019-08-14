a81eba9f7e6be163053f25781bbf5e413b78e46a
Andrew van Breda <support@avb-it.co.uk>
2019-08-14 18:22:49 +0100

    DGfM using a customised 5650 for distribution map. Note this cannot be used for other projects as it is customised.

--- js/proj4defs.js
+++ js/proj4defs.js
@@ -10,3 +10,7 @@ Proj4js.defs['EPSG:23030'] = "+proj=utm +zone=30 +ellps=intl +units=m +no_defs +
 Proj4js.defs["EPSG:31466"] = "+proj=tmerc +lat_0=0 +lon_0=6 +k=1 +x_0=2500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs";
 Proj4js.defs["EPSG:31467"] = "+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs";
 Proj4js.defs["EPSG:31468"] = "+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs";
+// For DGfM move central meridian from 15 to 14 - non-standard, also False_Easting: 33480000,0 (usually 33500000,0)
+// Standard definition commented out for reference
+// Proj4js.defs["EPSG:5650"] = "+proj=tmerc +lat_0=0 +lon_0=15 +k=0.9996 +x_0=33500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
+Proj4js.defs["EPSG:5650"] = "+proj=tmerc +lat_0=0 +lon_0=14 +k=0.9996 +x_0=33480000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
