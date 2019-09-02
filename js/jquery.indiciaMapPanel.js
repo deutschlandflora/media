f0555044b1ae2cfdb6b07bcacd8caec5e625b115
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-09-02 15:39:28 +0100

    A bit of linting

--- js/jquery.indiciaMapPanel.js
+++ js/jquery.indiciaMapPanel.js
@@ -2432,7 +2432,6 @@ var destroyAllFeatures;
      * Handle the automatic switching between layers for the dynamic layer.
      */
     function handleDynamicLayerSwitching(div) {
-      var thisZoomLevel = div.map.getZoom();
       var onLayer;
       var switcherChange = false;
       var baseLayer = div.map.baseLayer;
@@ -2440,30 +2439,25 @@ var destroyAllFeatures;
       // dynamicOSLeisureGoogleSat.0.
       var baseLayerIdParts = baseLayer.layerId.split('.');
       var onLayerIdx;
+      var dynamicLayers;
+      var bb;
+      var mapWidth;
       // Careful about recursion. Also don't bother if not on a dynamic layer.
       if (indiciaData.settingBaseLayer || typeof baseLayer.dynamicLayerIndex === 'undefined') {
         return;
       }
       // If we need to switch dynamic layer because of the zoom, find the new
       // sub-layer's index.
-      var dynamicLayers = _getPresetLayers(div.settings)[baseLayerIdParts[0]];
-      var bb = div.map.getExtent().transform(div.map.projection, new OpenLayers.Projection("EPSG:27700"));
-      var mapWidth = bb.right - bb.left;
-      onLayerIdx = dynamicLayers.reduce(function(index, lyr, i) {
+      dynamicLayers = _getPresetLayers(div.settings)[baseLayerIdParts[0]];
+      bb = div.map.getExtent().transform(div.map.projection, new OpenLayers.Projection('EPSG:27700'));
+      mapWidth = bb.right - bb.left;
+      onLayerIdx = dynamicLayers.reduce(function findLayer(index, lyr, i) {
         var mapLayer = lyr();
         if (!mapLayer.maxWidth || mapWidth < mapLayer.maxWidth) {
           return i;
-        } else {
-          return index;
         }
-      }, 0)
-      // if (baseLayer.maxZoom && thisZoomLevel > baseLayer.maxZoom) {
-      //   onLayerIdx = baseLayer.dynamicLayerIndex + 1;
-      // } else if (baseLayer.minZoom && thisZoomLevel < baseLayer.minZoom) {
-      //   onLayerIdx = baseLayer.dynamicLayerIndex - 1;
-      // } else {
-      //   onLayerIdx = baseLayer.dynamicLayerIndex;
-      // }
+        return index;
+      }, 0);
       indiciaData.settingBaseLayer = true;
       try {
         // Ensure switch is immediate.
